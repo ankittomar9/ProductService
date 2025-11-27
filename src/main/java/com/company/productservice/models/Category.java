27bed9851534976646c61c5feb2b4d10315d132e
tomar <tomarankit78@gmail.com>
2025-11-27 23:24:19 +0530

    Add Attributes to model Classes

--- src/main/java/com/company/productservice/models/Category.java
+++ src/main/java/com/company/productservice/models/Category.java
@@ -1,4 +1,11 @@
 package com.company.productservice.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class Category {
+    private String id;
+    private String title;
 }
